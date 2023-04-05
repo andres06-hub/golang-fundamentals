757465a46f5e501bcfd9783bdb5a2ab7e4891124
andres06-hub <andressierrarojas7@gmail.com>
2023-04-05 18:43:18 -0500

    feat: variables

---
+++ fundamentals/variables.go
@@ -0,0 +1,22 @@
+package main
+
+// Variable declaration. Variables must be declared before use.
+var x int
+var y float32
+var w float64
+var z bool
+
+// Variable assignment.
+x = 3
+y = 2.1
+w = 0.124545
+z = false
+
+// "Short" declarations use := to infer the type, declare, and assign.
+a := 4
+b := "Hello, world!"
+c := 1.2145
+d := 3.14195 // float64, an IEEE-754 64-bit floating point number.
+e := true
+f := false
+
